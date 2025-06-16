9e5cfdfd723384c6a00e0a001b6041363abe71e5
S H R E E <shreejalshah21@gmail.com>
2025-06-16 12:12:54 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-    const button = document.querySelector('.dark');
-button.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-});
-
 const menu = document.querySelector('.menu');
 const closeBtn = document.querySelector('.close-btn');
 const sidebar = document.querySelector('.sidebar');
 
 menu.addEventListener('click', () => {
-    sidebar.style.left = '0'; // Slide in
+    sidebar.style.right = '0'; // Slide in
 });
 
 closeBtn.addEventListener('click', () => {
-    sidebar.style.left = '-330px'; // Slide out
+    sidebar.style.right = '-330px'; // Slide out
 });
